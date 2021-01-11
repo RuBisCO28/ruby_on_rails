a11c71f57b3e6223fd6519b8eee367a2f9e83ebc
RuBisCO28 <54470624+RuBisCO28@users.noreply.github.com>
2021-01-12 02:26:17 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    #@user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # 保存の成功をここで扱う。
+      redirect_to @user # = redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    # paramsハッシュでは:user属性を必須とし、
+    # 名前、メールアドレス、パスワード、パスワードの確認の属性をそれぞれ許可し、それ以外を許可しない
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
